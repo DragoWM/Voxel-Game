02e3dd82b28ee9f7fc3e1a597c61e3bd1d63607e
Drago <scrymishi@gmail.com>
2025-04-24 23:56:10 +0100

    Basic Render Project Setup
    - completed basic project setup for terrain generator, terrain renderer and block list.
    - added some basic integration for terrain generator, terrain renderer and block list scripts
    - tested basic integration for terrain generator, terrain renderer and block list scripts by calling debug functions from world script.

--- Assets/Scripts/WorldScript.cs
+++ Assets/Scripts/WorldScript.cs
@@ -4,16 +4,50 @@ using UnityEngine;
 
 public class WorldScript : MonoBehaviour
 {
-
+    private BlockRegister blockList;
+    private TerrainGenerationBase terrainGenerator;
+    private TerrainRendererBase terrainRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        //ini scripts
+        getScriptReferences();
+
         //generate terrain
+        if (terrainGenerator == null)
+        {
+            Debug.Log("Terrain Gen Missing!");
+            return;
+        }
+
+        terrainGenerator.GenerateTerrainData();
 
         //render terrain
-        
+        terrainRenderer.renderTerrain();
 
+    }
+
+    void getScriptReferences()
+    {
+        //ini scripts
+        Transform child = transform.Find("BlockRegister");
+        if (child != null)
+        {
+            blockList = child.GetComponent<BlockRegister>();
+        }
+
+        child = transform.Find("TerrainGenerator");
+        if (child != null)
+        {
+            terrainGenerator = child.GetComponent<TerrainGenerationBase>();
+        }
+
+        child = transform.Find("TerrainRenderer");
+        if (child != null)
+        {
+            terrainRenderer = child.GetComponent<TerrainRendererBase>();
+        }
     }
 }
 
