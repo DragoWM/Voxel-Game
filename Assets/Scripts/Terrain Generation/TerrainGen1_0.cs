7f8d87d8cd0d380966afa83e1550dbbaed1ac935
Drago <scrymishi@gmail.com>
2025-04-24 23:08:04 +0100

    Project Layout Testing
    Testing modular approach and organising project accordingly.

---
+++ Assets/Scripts/Terrain Generation/TerrainGen1_0.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TerrainGen1_0 : TerrainGenerationBase
+{
+    public override void GenerateTerrainData()
+    {
+        Debug.Log("Basic Gen");
+    }
+}
---
+++ Assets/Scripts/WorldScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldScript : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //generate terrain
+
+        //render terrain
+        
+
+    }
+}
+
+
