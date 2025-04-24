c0ecf132f5ecc83143ccef203e9ecc84a08fe6d7
Drago <scrymishi@gmail.com>
2025-04-25 00:11:10 +0100

    comments
    Commented all scripts

--- Assets/Scripts/Blocks/BlockRegister.cs
+++ Assets/Scripts/Blocks/BlockRegister.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class BlockRegister : MonoBehaviour
 {
+    // dictionary that works off of a simple int as the ID and stores the custom BlockBase object
+    // int as the ID may be replaced in the future by int2,int3 or a float to make block variations/rotations easier to store
+    // will be refactored when adding block variations.
     public Dictionary<int, BlockBase> blockList = new Dictionary<int, BlockBase>();
 
     void Start()
     {
+        //register all block types, with their properties.
+        //will need to be refactored in the future, by loading data from a file for easier maintainence.
         registerBlock(-1, false, "Error");
         registerBlock(0, false, "Air");
         registerBlock(1, true, "Stone");
@@ -34,9 +39,12 @@ public class BlockRegister : MonoBehaviour
         }
     }
 
+    //basic function to return block instance
     public BlockBase returnBlock(int id)
     {
         BlockBase block = blockList[id];
+
+        //add a check to see if block exists and if not, return -1 (error block)
 
         return block;
     }
--- Assets/Scripts/Terrain Renderer/TerrainRendererBase.cs
+++ Assets/Scripts/Terrain Renderer/TerrainRendererBase.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+//renderer base class, allows for modular management of renderer.
 public abstract class TerrainRendererBase : MonoBehaviour
 {
     public abstract void renderTerrain();
